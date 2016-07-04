dc3bcb24333ea4cd2cdd036a9b2a3e9f9f4797d0
grawphook <grawphook@gmail.com>
2016-07-04 12:17:57 -0400

    Completed Chapter 2.  Creating Repeating Textures

--- Super Zombie Runner/Assets/Scripts/TiledBackground.cs
+++ Super Zombie Runner/Assets/Scripts/TiledBackground.cs
@@ -4,11 +4,13 @@ using System.Collections;
 public class TiledBackground : MonoBehaviour {
 
     public int textureSize = 32;
+    public bool scaleHorizontially = true;
+    public bool scaleVertically = true;
 
 	// Use this for initialization
 	void Start () {
-        var newWidth = Mathf.Ceil(Screen.width / (textureSize * PixelPerfectCamera.scale));
-        var newHeight = Mathf.Ceil(Screen.height / (textureSize * PixelPerfectCamera.scale));
+        var newWidth = !scaleHorizontially ? 1: Mathf.Ceil(Screen.width / (textureSize * PixelPerfectCamera.scale));
+        var newHeight = !scaleVertically ? 1: Mathf.Ceil(Screen.height / (textureSize * PixelPerfectCamera.scale));
 
         transform.localScale = new Vector3(newWidth * textureSize, newHeight * textureSize, 1);
 

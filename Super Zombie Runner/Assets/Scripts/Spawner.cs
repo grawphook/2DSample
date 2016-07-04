d62ab34f1532365ec2d243a854155451fef7d47c
grawphook <grawphook@gmail.com>
2016-07-04 15:49:12 -0400

    Chapter 3. Completed Section: Random spawner delay times

--- Super Zombie Runner/Assets/Scripts/Spawner.cs
+++ Super Zombie Runner/Assets/Scripts/Spawner.cs
@@ -6,9 +6,11 @@ public class Spawner : MonoBehaviour {
     public GameObject[] prefabs;
     public float delay = 2.0f;
     public bool active = true;
+    public Vector2 delayRange = new Vector2(1, 2);
 
 	// Use this for initialization
 	void Start () {
+        ResetDelay();
         StartCoroutine(EnemyGenerator());
 
 	}
@@ -22,13 +24,14 @@ public class Spawner : MonoBehaviour {
             var newTransform = transform;
 
             Instantiate(prefabs[Random.Range(0, prefabs.Length)], newTransform.position, Quaternion.identity);
+            ResetDelay();
         }
 
         StartCoroutine(EnemyGenerator());
     }
 	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    void ResetDelay()
+    {
+        delay = Random.Range(delayRange.x, delayRange.y);
+    }
 }
